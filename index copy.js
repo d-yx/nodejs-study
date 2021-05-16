000efd30298a71ce384b1daac9ed835c25e1064f
Dboss <352104389@qq.com>
2021-05-16 17:04:56 +0800

    update

---
+++ index copy.js
@@ -0,0 +1,9 @@
+// require("./init");
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+
+server.listen(9527, () => {
+  console.log("server listen on 9527");
+});
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-require("./init");
+// require("./init");
+const express = require("express");
+const app = express();
 
-const stuServe = require("./services/studentService");
-// stuServe.addStudent({
-//   name: "dfld",
-//   birthday: "2015-3-5",
-//   sex: true,
-//   mobile: "12617161222",
-//   ClassId: "5332",
-// }).catch(err=>{
-//     console.log(err)
-// });
+app.get("/news/:id", (req, res) => {
+  // 获取请求信息
+  console.log("请求头:", req.headers);
+  console.log("请求路径:", req.path);
+  console.log("请求参数:", req.query);
+  console.log("params:", req.params);
+  // 响应
+  // res.send("fdsfsd");
+  // res.setHeader("a", "123").end();
+  // 重定向(状态码302：临时重定向；状态码301：永久重定向)
+  // res.status(302).header("location", "http://dboss.icu").end();
+  // res.status(302).location("http://dboss.icu").end();
+  res.redirect(302, "http://dboss.icu"); //默认为301
+});
 
-stuServe.getStudents().then((res) => {
-  console.log(res);
+app.listen(9527, () => {
+  console.log("server listen on 9527");
 });
