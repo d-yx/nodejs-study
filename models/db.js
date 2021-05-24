c9690f85fadf9c783297b8e247287fe7346de4cf
Dboss <352104389@qq.com>
2021-05-24 16:20:38 +0800

    update

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { sqlLogger } = require("../logger");
-const sequelize = new Sequelize("myschooldb", "root", "dboss666", {
+const sequelize = new Sequelize("myschooldb", "root", "ybybdwyJ42.", {
   host: "localhost",
   dialect: "mysql",
   logging: (msg) => {
