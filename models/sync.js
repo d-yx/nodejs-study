699cd5f3915eb53d09b0cd69596e9c9c2b37a454
Dboss <352104389@qq.com>
2021-05-27 17:29:16 +0800

    update

--- models/sync.js
+++ models/sync.js
@@ -2,7 +2,7 @@
 require("./Admin");
 require("./Book");
 require("./Class");
-require("./Student");
+require("./Student"); 
 const sequelize = require("./db");
 sequelize.sync({ alter: true }).then(() => {
   console.log("所有模型同步完成");
